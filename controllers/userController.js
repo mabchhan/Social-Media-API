1cc6b45eed0d2387ea68b13a0e4cacbe9e115533
Mab <mabchhan@gmail.com>
2023-03-06 11:54:46 -0800

    added userController.js to controllers

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const { User } = require("../Models");
+
+module.exports = {
+  // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+};
