f4fbdc07687a83390536001270b06b7b335ad0b0
Mab <mabchhan@gmail.com>
2023-03-06 12:41:45 -0800

    added api/userRoutes.js to routes folder and changed some connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,15 @@
-const { connect, connection } = require("mongoose");
+// const { connect, connection } = require("mongoose");
 
-connect("mongodb://localhost/socila-media", {
+// connect("mongodb://localhost/social-media", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+const mongoose = require("mongoose");
+
+// Wrap Mongoose around local connection to MongoDB
+
+mongoose.connect("mongodb://127.0.0.1:27017/social-media", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-module.exports = connection;
+module.exports = mongoose.connection;
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ module.exports = {
   // Create a new user
   createUser(req, res) {
     User.create(req.body)
+
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
   },
